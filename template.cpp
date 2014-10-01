97f560def889aa1e660807237549fda068c38428
Jakub Borowski <prajmus@gmail.com>
2014-10-01 15:37:21 +0200

    Add template file

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map> // hash map
+#include <utility>
+#include <vector>
+
+#define DEBUG(x) do { cout << #x << " = " << x << '\n';}while(0)
+
+#define lli long long int
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    return 0;
+}
